e9bcd5f2f20888aa29e5db537699962035e30941
Lili Hoffman <ldhoffma@usc.edu>
2023-11-17 10:32:41 -0800

    edits

--- api.py
+++ api.py
@@ -19,4 +19,33 @@ max_radius_km = 1500
 url = requests.get(f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_time}&minmagnitude={min_magnitude}&latitude={latitude}&longitude={longitude}&maxradiuskm={max_radius_km}')
 dataset = url.json()
 
+features = dataset['features']
+
+places = []
+mags = []
+times = []
+lats = []
+lons = []
+weights = []
+
+for feature in features:
+    places.append(feature['properties']['place'])
+    mags.append(feature['properties']['mag'])
+    times.append(pd.to_datetime(feature['properties']['time'], unit='ms').strftime('%y/%m/%d %H:%M:%S'))
+    lats.append(feature['geometry']['coordinates'][1])
+    lons.append(feature['geometry']['coordinates'][0])
+    weights.append(feature['properties']['mag'])
+    
+turkey_coord = [39, 35]
+turkey_map_normal = folium.Map(location=turkey_coord, zoom_start=5.5)
+HeatMap(data=df[['Latitude', 'Longitude', 'Weight']], radius=15).add_to(turkey_map_normal)
+
+for index, row in df.iterrows():
+    folium.CircleMarker(
+        location=[row['Latitude'], row['Longitude']],
+        radius=row['Magnitude']/2,
+        color='red',
+        fill_color='red').add_to(turkey_map_normal)
+
+turkey_map_normal
 
