f2f00abb841a7ab5cd3160f81434ca0767b7434d
Lili Hoffman <ldhoffma@usc.edu>
2023-11-17 10:58:42 -0800

    edits

--- api.py
+++ api.py
@@ -12,8 +12,8 @@ pd.set_option('display.width', 500)
 
 start_time = 'now-180days'
 min_magnitude = 3
-latitude = 39.1458
-longitude = 34.1614
+latitude = 34.0224
+longitude = 118.2851
 max_radius_km = 1500
 
 url = requests.get(f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_time}&minmagnitude={min_magnitude}&latitude={latitude}&longitude={longitude}&maxradiuskm={max_radius_km}')
@@ -46,6 +46,8 @@ turkey_map_normal = folium.Map(location=turkey_coord, zoom_start=5.5)
 
 HeatMap(data=df[['Latitude', 'Longitude', 'Weight']], radius=15).add_to(turkey_map_normal)
 
+
+
 for index, row in df.iterrows():
     folium.CircleMarker(
         location=[row['Latitude'], row['Longitude']],
@@ -53,5 +55,5 @@ for index, row in df.iterrows():
         color='red',
         fill_color='red').add_to(turkey_map_normal)
 
-turkey_map_normal
+turkey_map_normal.save("heatmap_map.html")
 
