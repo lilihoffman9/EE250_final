4db98797cc45455815e45e5a11f494ae665ccf2e
Lili Hoffman <ldhoffma@usc.edu>
2023-11-17 10:51:48 -0800

    edits

--- api.py
+++ api.py
@@ -36,16 +36,22 @@ for feature in features:
     lons.append(feature['geometry']['coordinates'][0])
     weights.append(feature['properties']['mag'])
     
+data = {'Latitude': lats, 
+	'Longitude': lons,
+	'Weight': weights}
+df = pd.DataFrame(data)
+
 turkey_coord = [39, 35]
 turkey_map_normal = folium.Map(location=turkey_coord, zoom_start=5.5)
+
 HeatMap(data=df[['Latitude', 'Longitude', 'Weight']], radius=15).add_to(turkey_map_normal)
 
 for index, row in df.iterrows():
     folium.CircleMarker(
         location=[row['Latitude'], row['Longitude']],
-        radius=row['Magnitude']/2,
+        radius=row['Weight']/2,
         color='red',
         fill_color='red').add_to(turkey_map_normal)
 
-turkey_map_normal
+#turkey_map_normal
 
