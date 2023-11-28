c31d06115a73913bc642698faf2e5263310cafd0
ashly-usc <ashlylam@usc.edu>
2023-11-28 12:46:04 -0800

    Update ml.py

--- ml.py
+++ ml.py
@@ -3,7 +3,6 @@ import pandas as pd
 import folium
 from folium.plugins import HeatMap
 import pickle
-
 import numpy as np
 
 from matplotlib import pyplot as plt
@@ -12,19 +11,16 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 
-
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.float_format', lambda x: '%.3f' % x)
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('display.width', 500)
-
 start_time = 'now-180days'
 min_magnitude = 3
 latitude = 34.0224
 longitude = -118.3
 max_radius_km = 1500
-
 url = requests.get(f'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={start_time}&minmagnitude={min_magnitude}&latitude={latitude}&longitude={longitude}&maxradiuskm={max_radius_km}')
 dataset = url.json()
 
@@ -36,6 +32,10 @@ times = []
 lats = []
 lons = []
 weights = []
+rms = []
+sigs = []
+mage = []
+tsus = []
 
 for feature in features:
     places.append(feature['properties']['place'])
@@ -44,57 +44,71 @@ for feature in features:
     lats.append(feature['geometry']['coordinates'][1])
     lons.append(feature['geometry']['coordinates'][0])
     weights.append(feature['properties']['mag'])
-    
-data = {'Latitude': lats, 
+    rms.append(feature['properties']['rms'])
+    sigs.append(feature['properties']['sig'])
+    mage.append(feature['properties']['gap'])
+    tsus.append(feature['properties']['tsunami'])
+
+dataapi = {'Latitude': lats,
 	'Longitude': lons,
 	'Weight': weights}
-df = pd.DataFrame(data)
+dfapi = pd.DataFrame(dataapi)
 
-df.to_csv("output.csv", index=False)
+# dataml = {'Magnitude': mags,
+# 	'RMS': rms}
+dataml = {'Latitude': lats,
+	'Longitude': lons,
+	'MagE': mage,
+	'Magnitude': mags,
+	         'Tsu': tsus}
+dfml = pd.DataFrame(dataml)
+dfml.to_csv("output.csv", index=False)
 
-coins = pd.read_csv("output.csv")
-coins.head(10)
+plotdata = pd.read_csv("output.csv")
+plotdata.head(10)
 
-sns.scatterplot(x="mag",y="rms",data=coins)
+sns.scatterplot(x="Magnitude",y="Tsu",data=plotdata)
 
 # test train split
-X = coins[["mag"]].to_numpy()
-y = coins[["rms"]].to_numpy()
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = plotdata[['Latitude', 'Longitude']]
+y = plotdata[['Magnitude']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+print(X_train.shape, X_test.shape, y_train.shape, X_test.shape)
 
-## Import your favourite classifier and train it using the X_train data and hte y_train labels
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.datasets import make_classification
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.datasets import load_iris  # You can use any dataset you prefer
+# X = plotdata[["Magnitude"]].to_numpy()
+# y = plotdata[["RMS"]].to_numpy()
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
-## make sure you instantiate your classifier and name it clf
-clf = KNeighborsClassifier(n_neighbors=5)
-clf.fit(X_train, y_train)
+from sklearn.linear_model import LinearRegression
 
-## test the accuracy of your model
-y_pred = clf.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy:.2f}')
+# Train the linear regression model
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
 
-## plot the decision boundary of your classifier along with the scatterplot of the training data
-## hint: look at the ann example
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
+from sklearn.metrics import r2_score, mean_squared_error
 
-Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
+scores= {"Model name": ["Linear regression", "SVM", "Random Forest"], "mse": [], "R^2": []}
 
-plt.contourf(xx, yy, Z, alpha=0.4)
-plt.scatter(X[:, 0], X[:, 1], c=y, marker='o', edgecolor='k')
-plt.xlabel('Feature 1')
-plt.ylabel('Feature 2')
-plt.title(f'K-Nearest Neighbors (K={5})')
+# Predict on the testing set
+y_pred = regressor.predict(X_test)
+
+# Compute R^2 and MSE
+r2 = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+
+scores['mse'].append(mse)
+scores['R^2'].append(r2)
+
+print("R^2: {:.2f}, MSE: {:.2f}".format(r2, mse))
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Plot the regression line
+sns.regplot(x=X_test['Latitude'], y=y_test, color='blue', scatter_kws={'s': 10})
+sns.regplot(x=X_test['Longitude'], y=y_test, color='red', scatter_kws={'s': 10})
+plt.legend(labels=['Latitude', 'Longitude'])
+plt.title('Multiple Linear Regression Model')
+plt.xlabel('Predictor Variables')
+plt.ylabel('Magnitude')
 plt.show()
-
-# dump your classifier into a pickle file
-pickle.dump(clf, open("model.pickle","wb"))
-
-
