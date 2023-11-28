c521b9af534b409aac60e015f49aa75637e076b1
Lili Hoffman <ldhoffma@usc.edu>
2023-11-28 11:20:30 -0800

    edit

--- api.py
+++ api.py
@@ -9,11 +9,13 @@ broker_address = "eclipse.usc.edu"
 port = 1883
 topic = "sensor/data"
 radius = 1500
+make_map = 0
 
 # Callback when a message is received
 def on_message(client, userdata, msg):
     print(f"Received sensor data: {msg.payload.decode()}")
     radius = (int(msg.payload)*1000)
+    make_map = 1
     print(f"Set max radius: {radius}")
 
 pd.set_option('display.max_columns', None)
@@ -66,36 +68,36 @@ def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload) + "radius: " + msg.payload)
     # more callbacks, etc
  
+if make_map:
+	for feature in features:
+	    places.append(feature['properties']['place'])
+	    mags.append(feature['properties']['mag'])
+	    times.append(pd.to_datetime(feature['properties']['time'], unit='ms').strftime('%y/%m/%d %H:%M:%S'))
+	    lats.append(feature['geometry']['coordinates'][1])
+	    lons.append(feature['geometry']['coordinates'][0])
+	    weights.append(feature['properties']['mag'])
+	    
+	data = {'Latitude': lats, 
+		'Longitude': lons,
+		'Weight': weights}
+	df = pd.DataFrame(data)
 
-for feature in features:
-    places.append(feature['properties']['place'])
-    mags.append(feature['properties']['mag'])
-    times.append(pd.to_datetime(feature['properties']['time'], unit='ms').strftime('%y/%m/%d %H:%M:%S'))
-    lats.append(feature['geometry']['coordinates'][1])
-    lons.append(feature['geometry']['coordinates'][0])
-    weights.append(feature['properties']['mag'])
-    
-data = {'Latitude': lats, 
-	'Longitude': lons,
-	'Weight': weights}
-df = pd.DataFrame(data)
+	turkey_coord = [latitude, longitude]
+	turkey_map_normal = folium.Map(location=turkey_coord, zoom_start=5.5)
 
-turkey_coord = [latitude, longitude]
-turkey_map_normal = folium.Map(location=turkey_coord, zoom_start=5.5)
-
-HeatMap(data=df[['Latitude', 'Longitude', 'Weight']], radius=15).add_to(turkey_map_normal)
+	HeatMap(data=df[['Latitude', 'Longitude', 'Weight']], radius=15).add_to(turkey_map_normal)
 
 
 
-for index, row in df.iterrows():
-    folium.CircleMarker(
-        location=[row['Latitude'], row['Longitude']],
-        radius=row['Weight']/2,
-        color='red',
-        fill_color='red').add_to(turkey_map_normal)
+	for index, row in df.iterrows():
+	    folium.CircleMarker(
+		location=[row['Latitude'], row['Longitude']],
+		radius=row['Weight']/2,
+		color='red',
+		fill_color='red').add_to(turkey_map_normal)
 
-turkey_map_normal.save("heatmap_map.html")
-print("Created map")
+	turkey_map_normal.save("heatmap_map.html")
+	print("Created map")
 
 # Start the MQTT loop to listen for messages
 client.loop_forever()
